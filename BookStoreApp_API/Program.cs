7526360f1be3f3c3258b430a2e858f2204f8c2bc
Ismail-Starke <165874637+Ismail-Starke@users.noreply.github.com>
2024-04-30 08:46:37 +0200

    Added EF Core and Scaffolded Database

--- BookStoreApp_API/Program.cs
+++ BookStoreApp_API/Program.cs
@@ -1,8 +1,12 @@
+using BookStoreApp_API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

77919a2fd01ae019170f0799c8fc1c0290e6209a
Ismail-Starke <165874637+Ismail-Starke@users.noreply.github.com>
2024-05-02 10:43:18 +0200

    Book API Controller Setup

--- BookStoreApp_API/Configurations/MapperConfig.cs
+++ BookStoreApp_API/Configurations/MapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BookStoreApp_API.Data;
 using BookStoreApp_API.Models.Author;
+using BookStoreApp_API.Models.Book;
 
 namespace BookStoreApp_API.Configurations
 {
@@ -8,9 +9,21 @@ namespace BookStoreApp_API.Configurations
     {
         public MapperConfig()
         {
+            // Author Mappings
             CreateMap<AuthorCreateDTO, Author>().ReverseMap();
             CreateMap<AuthorUpdateDTO, Author>().ReverseMap();
-            CreateMap<AuthorReadOnlyDTO, Author>().ReverseMap();
+
+            // Book Mappings
+            CreateMap<BookCreateDTO, Book>().ReverseMap();
+            CreateMap<BookUpdateDTO, Book>().ReverseMap();
+
+            CreateMap<Book, BookReadOnlyDTO>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
+
+            CreateMap<Book, BookDetailsDTO>()
+                .ForMember(q => q.AuthorName, d => d.MapFrom(map => $"{map.Author.FirstName} {map.Author.LastName}"))
+                .ReverseMap();
         }
     }
 }
---
+++ BookStoreApp_API/Models/Book/BookReadOnlyDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace BookStoreApp_API.Models.Book
+{
+    public partial class BookReadOnlyDTO : BaseDTO
+    {
+        public int Title { get; set; }
+
+        public string? Image { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string AuthorName { get; set; }
+    }
+}
